5cc74d98b47762bc6bd2d8c4f75dca7e2187fbe2
skiparmler <johan.parmler@kvalitetsindex.se>
2017-03-20 16:00:22 +0100

    add graph

--- Assignment2.py
+++ Assignment2.py
@@ -33,17 +33,36 @@ df1 = df1.drop(df1.index[[59]])
 df2 = df2.drop(df2.index[[59]])
 df1.index =range(365)
 df2.index =range(365)
+df3.index =range(365)
+df4.index =range(365)
 
-
+df3[df3>df2]=np.nan
+df4[df4<df1]=np.nan
+   
 
 import matplotlib.pyplot as plt
-plt.figure()
+fig_size[0] = 12
+fig_size[1] = 9
+
 # plot the linear data and the exponential data
 maxV = df1['TMAX']
 minV = df2['TMIN']
 minV15 = df3['TMIN']
 maxV15 = df4['TMAX']
+maxV.columns = ['maximum 2005 - 14']
+minV.columns = ['minimum 2005 - 14']
+maxV15.columns = ['maximum 2015']
+minV15.columns = ['minimum 2015']
+minV.renames = "hej"
+plt.figure()
 plt.plot(maxV, '-', minV, '-')
 plt.fill_between(range(365), minV, maxV, facecolor='blue', alpha=0.25)
 plt.scatter(range(365), minV15)
 plt.scatter(range(365), maxV15)
+plt.ylabel('Temperature')
+plt.xlabel('Day of the year')
+plt.title('Record of high and low temperatures by day of the year')
+plt.legend(loc="upper left", bbox_to_anchor=[0, 1], ncol=2, shadow=True, title="Legend", fancybox=True)
+
+
+
